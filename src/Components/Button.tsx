7bbe5a61edc50d3b74e5250f1c6e57c8f805078a
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2024-02-01 08:55:39 +0100

    chore: added extra feats to the buttonComponent

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,19 +1,35 @@
-import React, { ButtonHTMLAttributes} from 'react'
+import React, { ButtonHTMLAttributes, useState} from 'react'
 import '../Styles/ComponentStyles/Button.scss'
 
 interface ButttonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   value: string;
   color: string;
   bgColor: string;
+  bgHoverColor: string;
+  textHoverColor: string;
+  borderHoverColor: string;
+  borderHoverStyle?: string; 
 }
 
-const Button: React.FC<ButttonProps> = ({value, color, bgColor}) => {
-  const buttonStyle = {
-    color: color,
-    backgroundColor: bgColor,
+const Button: React.FC<ButttonProps> = ({value, color, bgColor, bgHoverColor, textHoverColor, borderHoverColor, borderHoverStyle}) => {
+  const [isHovered,setIsHovered] = useState(false);
+  // const [hoverColor, setHoverColor] = useState(color)
+
+  const handleMouseEnter =  () => {
+    setIsHovered(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  }
+  
+  const buttonStyle: React.CSSProperties = {
+    backgroundColor: isHovered ? bgHoverColor : bgColor,
+    color: isHovered ? textHoverColor : color,
+    borderColor: isHovered ? borderHoverColor : "initial",
   }
   return ( 
-    <button className='button' style={buttonStyle}>
+    <button className='button' style={buttonStyle} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {value}
     </button>
    );
